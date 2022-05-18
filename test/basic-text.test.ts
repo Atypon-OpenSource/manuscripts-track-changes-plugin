aedd12be260728e629ee1a5ac99c51d09f3b7e3e
Teemu <tkoivisto456@gmail.com>
2022-05-18 13:11:08 +0300

    fix(track): fix missing jest types in vscode

--- test/basic-text.test.ts
+++ test/basic-text.test.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/// <reference types="@types/jest" />;
 import { promises as fs } from 'fs'
 
 import { setAction, TrackChangesAction, trackCommands } from '../src'
