b5e6a5a58e760728e9854e1a29aa944b150055ef
Teemu Koivisto <tkoivisto456@gmail.com>
2022-09-23 16:26:33 +0300

    test(track): update manuscript.test snapshot

--- test/manuscript/manuscript.test.ts
+++ test/manuscript/manuscript.test.ts
@@ -72,12 +72,12 @@ describe('manuscript.test', () => {
         }
       })
 
-    // await fs.writeFile('test.json', JSON.stringify(tester.toJSON()))
 
     tester.cmd(trackCommands.applyAndRemoveChanges())
 
+    // await fs.writeFile('test.json', JSON.stringify(tester.toJSON()))
     expect(tester.toJSON()).toEqual(manuscriptApplied[0])
-    expect(uuidv4Mock.mock.calls.length).toBe(14)
+    expect(uuidv4Mock.mock.calls.length).toBe(10)
     expect(tester.trackState()?.changeSet.hasInconsistentData).toEqual(false)
     expect(log.warn).toHaveBeenCalledTimes(0)
     expect(log.error).toHaveBeenCalledTimes(0)
