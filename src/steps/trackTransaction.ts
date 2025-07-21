a21e5e5502b7212a40bb99722c7c2c037f8a55b6
Noura Ghazzawi <nghazzawi@atypon.com>
2025-07-21 14:40:19 +0300

    LEAN-4611: MFP-020 - Reorder pictures within a figure- track move pen… (#65)
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/steps/trackTransaction.ts
+++ src/steps/trackTransaction.ts
@@ -116,7 +116,7 @@ export function trackTransaction(
   const cleanSteps = filterMeaninglessMoveSteps(tr, movingStepsAssociated)
 
   for (let i = cleanSteps.length - 1; i >= 0; i--) {
-    const step = tr.steps[i]
+    const step = cleanSteps[i]
 
     log.info('transaction step', step)
     iters += 1
