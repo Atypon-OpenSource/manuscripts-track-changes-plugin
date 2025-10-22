c125992df3c9c41a8729fd79686f48267eb27c20
mbartenev-atypon <mbartenev@atypon.com>
2025-10-22 14:47:22 +0200

    removed duplicated implementation

--- src/tracking/change-step/diffChangeSteps.ts
+++ src/tracking/change-step/diffChangeSteps.ts
@@ -78,24 +78,3 @@ export function diffChangeSteps(steps: ChangeStep[]) {
   log.info('FINISH DIFF: ', [...updatedDeleted, ...updated])
   return [...updatedDeleted, ...updated]
 }
-
-export function mapChangeSteps(steps: ChangeStep[], mapping: Mapping) {
-  steps.forEach((step) => {
-    if ('from' in step) {
-      step.from = mapping.map(step.from)
-    }
-    if ('to' in step) {
-      step.to = mapping.map(step.to)
-    }
-    if ('pos' in step) {
-      step.pos = mapping.map(step.pos)
-    }
-    if ('nodeEnd' in step) {
-      step.nodeEnd = mapping.map(step.nodeEnd)
-    }
-    if ('mergePos' in step) {
-      step.mergePos = mapping.map(step.mergePos)
-    }
-  })
-  return steps
-}
