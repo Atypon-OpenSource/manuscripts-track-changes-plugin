c4e217604e3c286d41d23c96603b3068ef588552
mbartenev-atypon <mbartenev@atypon.com>
2025-11-03 14:28:59 +0100

    updates

--- src/changeHelpers/revertChange.ts
+++ src/changeHelpers/revertChange.ts
@@ -79,7 +79,12 @@ export function revertWrapNodeChange(tr: Transaction, change: IncompleteChange, 
     tr.doc.nodesBetween(from, to, (node, pos) => {
       const $fromPos = tr.doc.resolve(tr.mapping.map(pos))
       const $toPos = tr.doc.resolve(tr.mapping.map(pos + node.nodeSize - 1))
-      const nodeRange = $fromPos.blockRange($toPos)
+      const nodeRange = $fromPos.blockRange(
+        $toPos,
+        (node) =>
+          // this to make sure we apply lift to compatible content with change node type
+          !!(change as NodeChange).node?.type.contentMatch.matchType(node.type)
+      )
       if (!nodeRange) {
         return
       }
