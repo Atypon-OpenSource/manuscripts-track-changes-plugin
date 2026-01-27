929e1d49ed4ec711856e657c7caccbd7aed827e3
mbartenev-atypon <mbartenev@atypon.com>
2026-01-27 20:23:22 +0100

    wip

--- src/utils/shared-utils.ts
+++ src/utils/shared-utils.ts
@@ -1,7 +1,23 @@
+/*!
+ * © 2026 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { schema } from '@manuscripts/transform'
 import { Attrs, Fragment, Mark, Node as ProsemirrorNode } from 'prosemirror-model'
-import { CHANGE_OPERATION, TrackedAttrs } from '../types/change'
+
 import { isShadowDelete } from '../tracking/steps-trackers/qualifiers'
+import { CHANGE_OPERATION, TrackedAttrs } from '../types/change'
 
 export function isDeleted(node: ProsemirrorNode | Mark) {
   if (node.attrs.dataTracked) {
